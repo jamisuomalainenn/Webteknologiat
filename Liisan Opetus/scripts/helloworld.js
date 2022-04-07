6eb42668f54ec3f083d0a15ec604dca1aebede1f
jamisuomalainenn <Jami.Suomalainen@student.lab.fi>
2022-04-07 13:48:21 +0300

    kartta valmis

--- Liisan Opetus/scripts/helloworld.js
+++ Liisan Opetus/scripts/helloworld.js
@@ -1,11 +1,17 @@
+var helloElement = document.getElementById('hello-div');
+
 window.onload = function () {
     console.log('Moro mään')
 }
 
 function sayHelloTo(name) {
-    
+
     console.log("sayHello-funktio suoritettu");
-    alert("Hei " + name + "!");
-    
-    }
-    
+    //alert("Hei " + name + "!");
+    document.getElementById('hello-div').innerHTML = 'Hei ' + name;
+
+}
+
+function hideText() {
+    helloElement.innerHTML = ' ';
+}
--- Liisan Opetus/scripts/map.js
+++ Liisan Opetus/scripts/map.js
@@ -1,11 +1,17 @@
+let alku = "https://www.google.com/maps?q="
+let osoite = ""
+let kaupunki = ""
+let loppu = "&output=embed"
+
+function myFunction() {
+    osoite = document.getElementById("osoiteArvo").value;
+    kaupunki = document.getElementById("kaupunkiArvo").value;
+
+    (document.getElementById("inlineFrameExample").src) = alku + osoite + kaupunki + loppu;
+}
 var pElement;
 
 window.onload = function () {
     pElement = document.getElementById
     src = "'https://www.google.com/maps?q=' + Yliopistonkatu 32 + Lappeenranta + '&output=embed'"
-}
-
-function myFunction() {
-
-
 }
