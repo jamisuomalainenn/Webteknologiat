c9c21cb6dffec062e03a84250fecefccf5fd3d94
jamisuomalainenn <Jami.Suomalainen@student.lab.fi>
2022-04-07 13:17:35 +0300

    .

--- Liisan Opetus/scripts/map.js
+++ Liisan Opetus/scripts/map.js
@@ -1,9 +1,11 @@
+var pElement;
+
+window.onload = function () {
+    pElement = document.getElementById
+    src = "'https://www.google.com/maps?q=' + Yliopistonkatu 32 + Lappeenranta + '&output=embed'"
+}
+
+function myFunction() {
 
 
-window.onload = function(){ 
-    src="'https://www.google.com/maps?q=' + Yliopistonkatu 32 + Lappeenranta + '&output=embed'"}
-
-    function myFunction () {
-
-        
-    }
+}
