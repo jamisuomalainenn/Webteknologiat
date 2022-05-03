389f16681b3a3a170a9a96ae3262c223af633b9b
jamisuomalainenn <Jami.Suomalainen@student.lab.fi>
2022-05-03 22:48:16 +0300

    ajax valmis

--- Liisan Opetus/scripts/jokes.js
+++ Liisan Opetus/scripts/jokes.js
@@ -1,45 +1,44 @@
-let i = 0;
-let item = 0;
-document.getElementById("gif").style.display = "none";
-function fetchApi() {
-    document.getElementById("gif").style.display = "block";
-    fetch('https://api.icndb.com/jokes/random'), {
-        url: urlNorrisJokes,
-        type: "GET",
-        dataType: "json",
-        success: function (response) { }
-            .then(response => response.json())
-            .then(data => {
+var counter = 0;
+var index = 0;
+$("#gif").hide();
 
+$("#btn").click(function () {
+    let urlNorrisJokes = "https://api.icndb.com/jokes/random";
+    index++;
+    $("#gif").show();
 
-                i++;
-                item++;
-                let data =
-                    '<div class="accordion-item">'
-                    + '<h2 class="accordion-header" id="flush-heading' + i + '">'
-                    + '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse' + i + '" aria-expanded="false" aria-controls="flush-collapse' + i + '">'
-                    + 'Juttu ' + item;
-                + '</button>'
+    setTimeout(() => {
+        $.ajax({
+            url: urlNorrisJokes,
+            type: "GET",
+            dataType: "json",
+            success: function (response) {
+                // show data in DIV
+
+                // Bootstrap
+                let data = '<div class="accordion-item">'
+                    + '<h2 class="accordion-header" id="flush-heading' + index + '">'
+                    + '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse' + index + '" aria-expanded="false" aria-controls="flush-collapse' + index + '">'
+                    + 'Vitsi' + index
+                    + '</button>'
                     + '</h2>'
-                    + '<div id="flush-collapse' + i + '" class="accordion-collapse collapse" aria-labelledby="flush-heading' + i + '" data-bs-parent="#accordionFlushExample">'
+                    + '<div id="flush-collapse' + index + '" class="accordion-collapse collapse" aria-labelledby="flush-heading' + index + '" data-bs-parent="#accordionFlushExample">'
                     + response.value.joke
                     + '</div>';
                 document.getElementById("accordionFlushExample").innerHTML += data;
-                document.getElementById("gif").style.display = "none";
-            }
-
-            )
-    }
-
-
-
-    var clicks = 0;
-
-    function counter() {
-        clicks += 1;
-        document.getElementById("clicks").innerHTML = clicks;
-    };
+                $("#gif").hide();
+                counter++;
+                $("#clicks").text(counter);
+            },
+            error: function (jqxhr, textStatus, error) {
+                console.log("Error: " + textStatus);
+                //$("#flush-collapseOne").append("<br>" + "<i>" + "Ei löydy" + "</i>");
+            },
+        });
+    }, "2000");
+});
 
 
-    document.body.style.backgroundImage = "url('images/maalaus.jpg')";
-    document.body.style.backgroundSize = "cover"
+
+document.body.style.backgroundImage = "url('images/maalaus.jpg')";
+document.body.style.backgroundSize = "cover"
