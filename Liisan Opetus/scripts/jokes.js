fa7225c505029a5725148a8f638a07ed38123230
jamisuomalainenn <Jami.Suomalainen@student.lab.fi>
2022-05-03 11:28:03 +0300

    ajax, kesken

--- Liisan Opetus/scripts/jokes.js
+++ Liisan Opetus/scripts/jokes.js
@@ -1,17 +1,19 @@
 let i = 0;
+let item = 0;
 document.getElementById("gif").style.display = "none";
-function moi() {
+function fetchApi() {
     document.getElementById("gif").style.display = "block";
-    fetch('http://numbersapi.com/42')
+    fetch('http://numbersapi.com/2012/year?write&fragment')
         .then(response => response.text())
         .then(data => {
             i++;
+            item++;
             let itemdata =
                 '<div class="accordion-item">'
                 + '<h2 class="accordion-header" id="flush-heading' + i + '">'
                 + '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse' + i + '" aria-expanded="false" aria-controls="flush-collapse' + i + '">'
-                + 'Accordion Item #1'
-                + '</button>'
+                + 'Juttu ' + item;
+            + '</button>'
                 + '</h2>'
                 + '<div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-heading' + i + '" data-bs-parent="#accordionFlushExample">'
                 + '<div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first items accordion body.</div>'
@@ -25,13 +27,6 @@ function moi() {
 }
 
 
-
-
-
-// Bootstrap
-
-
-//document.getElementById("gif").style.display = block;
 
 var clicks = 0;
 
