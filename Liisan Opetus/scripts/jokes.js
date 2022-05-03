ee15772264bf5644cdb9125f8110f4e11319d356
jamisuomalainenn <Jami.Suomalainen@student.lab.fi>
2022-05-03 23:00:42 +0300

    kurssi valmis

--- Liisan Opetus/scripts/jokes.js
+++ Liisan Opetus/scripts/jokes.js
@@ -19,7 +19,7 @@ $("#btn").click(function () {
                 let data = '<div class="accordion-item">'
                     + '<h2 class="accordion-header" id="flush-heading' + index + '">'
                     + '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse' + index + '" aria-expanded="false" aria-controls="flush-collapse' + index + '">'
-                    + 'Vitsi' + index
+                    + 'Vitsi ' + index
                     + '</button>'
                     + '</h2>'
                     + '<div id="flush-collapse' + index + '" class="accordion-collapse collapse" aria-labelledby="flush-heading' + index + '" data-bs-parent="#accordionFlushExample">'
